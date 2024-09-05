9a0ce8c894b3168cac8417e728b3b4e9b02840b8
Andrew <drewm8080@gmail.com>
2024-09-04 17:12:39 -0700

    update to header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,21 @@
+import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/header';
 import Search from './components/search';
-import {useState} from 'react';
 
 
+const App = () => {
+  const [word, setWord] = useState('');
 
-function App() {
-  const [word,setWord] = useState('');
-  function handleSearchSubmit(e) {
-    e.preventDefault()
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
     console.log(word);
   }
+
   return (
     <div>
       <Header title="Images Gallery"/>
-      <Search word ={word} setWord= {setWord} handleSubmit = {handleSearchSubmit}/>
+      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit}/>
     </div>
   );
 }
